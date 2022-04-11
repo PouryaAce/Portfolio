7c3d501f80ebe72ecd00919967f3fd82ab7c3efc
PouryaAce <pouriaghafour@gmail.com>
2022-04-11 12:10:26 +1000

    final--commit

--- src/components/header/CTA.js
+++ src/components/header/CTA.js
@@ -4,7 +4,12 @@ import Resume from "../../assets/resume.pdf";
 const CTA = () => {
   return (
     <div className="cta">
-      <a href={Resume} download className="btn">
+      <a
+        href="https://drive.google.com/file/d/1Jom3VF3CEnpD4pbaCuOdarfsFbhQ39ZY/view?usp=sharing"
+        download
+        className="btn"
+        target="_blank"
+      >
         Download Resume
       </a>
       <a href="#contact" className="btn btn-primary">
